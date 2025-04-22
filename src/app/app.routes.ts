a6154f4d59a92fd8e023870e79b2eec5f3b1005a
Guilherme Henrique da Silva <guilhermehenriquesilva.dev@gmail.com>
2025-04-22 17:10:57 -0300

    cria rotas iniciais

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponent
+    },
+    {
+        path: "home",
+        component: HomeComponent
+    },
+    {
+        path: "404",
+        component: PageNotFoundComponent
+    },
+    {
+        path: "**",
+        redirectTo: "/404"
+    }
+];
