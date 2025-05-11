355315aa3242f611b6c63703fa1fe70bdc8c7432
Guilherme Henrique da Silva <guilhermehenriquesilva.dev@gmail.com>
2025-05-10 21:03:27 -0300

    cria tabela de laudos

--- src/app/components/laudos/laudos.component.ts
+++ src/app/components/laudos/laudos.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 import { TopNavbarComponent } from '../top-navbar/top-navbar.component';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-laudos',
-  imports: [TopNavbarComponent],
+  imports: [TopNavbarComponent, NgFor, NgIf],
   templateUrl: './laudos.component.html',
   styleUrl: './laudos.component.css'
 })
 export class LaudosComponent {
+
+  nomePaciente = 'Fulano da Silva';
+
+  tableColumns = ['Data', 'Descrição', 'CID', '', ''];
+
+  laudosPaciente = [
+    {
+      data: '13/01/2025',
+      descricao: 'Depressão',
+      cid: 'F32.0'
+    },
+    {
+      data: '10/02/2025',
+      descricao: 'Depressão',
+      cid: 'F32.0'
+    },
+    {
+      data: '10/03/2025',
+      descricao: 'Depressão',
+      cid: 'F32.1'
+    }
+  ];
 
 }
