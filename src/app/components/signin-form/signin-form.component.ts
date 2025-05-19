c8b335d336c9bc566611186b87dbd16b0f23ac01
Guilherme Henrique da Silva <guilhermehenriquesilva.dev@gmail.com>
2025-05-19 18:24:19 -0300

    ajusta componente de cadastro

---
+++ src/app/components/signin-form/signin-form.component.ts
@@ -0,0 +1,41 @@
+import { AfterViewInit, Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-signin-form',
+  imports: [],
+  templateUrl: './signin-form.component.html',
+  styleUrl: './signin-form.component.css'
+})
+export class SigninFormComponent implements AfterViewInit{
+
+  constructor(private router: Router) {}
+
+    estadosItems = [
+      'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA',
+      'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PB', 'PI', 'RJ', 'RN',
+      'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'
+    ]
+
+  ngAfterViewInit(): void {
+    // Aplicar validação customizada do Bootstrap
+    const forms = document.querySelectorAll<HTMLFormElement>('.needs-validation');
+    const btnLogin = document.querySelector<HTMLFormElement>('#btnSignin');
+
+    forms.forEach((form) => {
+      form.addEventListener('submit', (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+
+    addEventListener("submit", (event) => {
+      alert('Olá! Vamos buscar na nossa base esse usuário e senha...');
+      this.router.navigate(['/home']);
+    })
+  }
+
+}
--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,41 +1,12 @@
-import { AfterViewInit, Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { SigninFormComponent } from '../signin-form/signin-form.component';
 
 @Component({
   selector: 'app-signin',
-  imports: [],
+  imports: [SigninFormComponent],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
-export class SigninComponent implements AfterViewInit {
-
-  constructor(private router: Router) {}
-
-    estadosItems = [
-      'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA',
-      'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PB', 'PI', 'RJ', 'RN',
-      'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'
-    ]
-
-  ngAfterViewInit(): void {
-    // Aplicar validação customizada do Bootstrap
-    const forms = document.querySelectorAll<HTMLFormElement>('.needs-validation');
-    const btnLogin = document.querySelector<HTMLFormElement>('#btnSignin');
-
-    forms.forEach((form) => {
-      form.addEventListener('submit', (event) => {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-
-    addEventListener("submit", (event) => {
-      alert('Olá! Vamos buscar na nossa base esse usuário e senha...');
-      this.router.navigate(['/home']);
-    })
-  }
+export class SigninComponent {
 
 }
